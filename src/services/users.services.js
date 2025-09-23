c83fbc8409e5389b5abffc855cee94725d1f5f8f
mayowa1039 <mayowa1039@yahoo.com>
2025-09-23 11:09:28 +0100

    Update user model and services to include role; enhance createUser function with password hashing and JWT token generation

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -3,13 +3,14 @@ import User from '../models/user.js';
 
 
 
-export const createNewUser = async (name, email, password, phoneNumber, address) => {
+export const createNewUser = async (name, email, password, phoneNumber, address, role) => {
     const newUser = new User ({
         name: name,
         email: email,
         password: password,
         phoneNumber: phoneNumber,
-        address: address
+        address: address,
+        role: role
     })
     await newUser.save();
     return newUser 
