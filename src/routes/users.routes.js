0534959cbecea3512ffa059fcbc6fe71312417ee
mayowa-1039 <mayowa1039@yahoo.com>
2025-09-24 11:12:59 +0100

    Refactor user routes to include getAllUsers functionality; remove merge conflict markers.

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,21 +1,12 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import { createUser } from "../controllers/users.controllers.js";
 import { LoginUser } from "../controllers/users.controllers.js";
-=======
-import { createUser, getALLUsers } from "../controllers/users.controllers.js";
->>>>>>> 228981815f7f45f2d9238abdc11f9b899a9a1a62
 
 
 const router = Router();
 
-<<<<<<< HEAD
 router.post("/",createUser);
 router.post("/login", LoginUser);
-=======
-router.post("/", createUser);
-router.get("/", getALLUsers)
->>>>>>> 228981815f7f45f2d9238abdc11f9b899a9a1a62
 
 router.get('/', async (req, res) => {
     res.json({ User: req.User });
